52447b2ba2869c4ab38fd49836ee71a9174be5b3
m.aldajaani <m.aldajaani@lean.sa>
2022-10-03 14:20:09 +0300

    Initial Commit

---
+++ test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by Maha Aldajaani on 07/03/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
